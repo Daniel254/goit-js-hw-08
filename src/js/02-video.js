8950fb370ecc74a73a2bd8f626e5d7b2aae3a659
Daniel254 <Daniel.laskin@gmail.com>
2022-04-30 17:36:02 +0300

    Exercise 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const videoplayerCurrentTime = localStorage.getItem('videoplayer-current-time');
 
+const onFirstLoad = () => {
+  if (!videoplayerCurrentTime) {
+    return;
+  }
+  player.setCurrentTime(videoplayerCurrentTime);
+};
+const onTimeUpdate = data => {
+  console.log('timeupdate data', data.seconds);
+
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+addEventListener('DOMContentLoaded', onFirstLoad);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
